8ce3d0d503f571827162523260fbbc93aa4faff2
Edubew <edubew98@gmail.com>
2022-07-20 20:44:46 +0300

    Add About component and styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 // eslint-disable-next-line  import/extensions
 import Info from './components/Info';
+import About from './components/About';
 
 export class App extends Component {
   render() {
     return (
       <div className="main-container">
         <Info />
+        <About />
       </div>
     );
   }
