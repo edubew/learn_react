e529a614fa0c20ec7ce178bfee3970b5989bda8b
Edubew <edubew98@gmail.com>
2022-07-20 20:00:09 +0300

    Add name and buttons to the info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import edube from './images/edube.jpg';
+import edube from '../images/edube.jpg';
 
 export class Info extends Component {
   render() {
     return (
-      <div>
+      <div className="info">
         <img src={edube} alt="My profile" />
+        <h1>Winfred Edube</h1>
+        <h2>FrontEnd Developer</h2>
+        <div className="buttons-container">
+          <a href="edubew98@gmail.com">Email</a>
+          <a href="edubew98@gmail.com">LinkedIn</a>
+        </div>
       </div>
     );
   }
