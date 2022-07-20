e796e50b72a04b30dc599960476cbddafbabcd4d
Edubew <edubew98@gmail.com>
2022-07-20 18:15:45 +0300

    Add styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Main from './components/Main';
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="main-container">
         <Navbar />
         <Main />
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,23 @@ import React, { Component } from 'react';
 export class Main extends Component {
   render() {
     return (
-      <div>
-        <h2>ReactFacts</h2>
-        <h2>React Course - Project 1</h2>
-        <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first realesed in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on Github</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </div>
+      <main>
+        <div className="heading">
+          <h2>ReactFacts</h2>
+          <h2>React Course - Project 1</h2>
+        </div>
+        <div className="main-body">
+          <h1>Fun facts about React</h1>
+          <ul className="facts">
+            <li>Was first realeased in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprice apps, including mobile apps</li>
+          </ul>
+        </div>
+
+      </main>
     );
   }
 }
